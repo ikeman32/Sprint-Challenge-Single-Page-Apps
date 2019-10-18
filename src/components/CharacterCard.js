8daee74e5f90ae30508e50d599e5ad8fe3c7c2a7
David Isakson <ikeman32@yahoo.com>
2019-10-18 10:28:16 -0700

    Character Cards Rendering along with welcome page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Cards = styled.div`
+display: Flex;
+margin-bottom: 1rem;
+border: 1px solid grey;
+
+`;
+
+const Bio = styled.div`
+margin-left: 1rem;
+`;
+
+export default function CharacterCard(props) {
+  return(
+    <Cards>
+      <img src={props.image}/>
+      <Bio>
+        <h1>{props.name}</h1>
+        <p>{props.species}</p>
+      </Bio>
+    </Cards>
+  );
 }
