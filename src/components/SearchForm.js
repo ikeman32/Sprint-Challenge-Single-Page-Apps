ca168c97d50005ed2af8daf214578b56f4e9a004
David Isakson <ikeman32@yahoo.com>
2019-10-18 10:58:01 -0700

    search form not finished yet

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
  
+  const[searchTerm, setSearchTerm] = useState('');
+  const[searchResults, setSearchResults] = useState([]);
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() =>{
+    const results = array.filter(char =>
+      char.toLowerCase().includes(searchTerm)
+      );
+  },[searchTerm])
   return (
     <section className="search-form">
-     // Add a search form here
+     <input 
+      type='text' 
+      placeholder="Search"
+      value={searchTerm}
+      onChange={handleChange}/>
     </section>
   );
 }
